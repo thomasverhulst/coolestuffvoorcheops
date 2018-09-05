e9c151ac63f7b37c2a8eb914f97eacf75afacb4b
Thomas.Verhulst <Thomas.Verhulst@850G5-TVE.cheops.be>
2018-09-05 15:59:38 +0200

    More openId stuff

--- src/main/java/com/tv/tutorials/coolestuffvoorcheops/CoolestuffvoorcheopsApplication.java
+++ src/main/java/com/tv/tutorials/coolestuffvoorcheops/CoolestuffvoorcheopsApplication.java
@@ -3,8 +3,13 @@ package com.tv.tutorials.coolestuffvoorcheops;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
 
 @SpringBootApplication
 //@EnableJpaRepositories("com.tv.tutorials.coolestuffvoorcheops.repositories")
@@ -38,5 +43,18 @@ public class CoolestuffvoorcheopsApplication {
 
 	   };
 
+	}
+	// nodig voor openid?
+	@EnableGlobalMethodSecurity(prePostEnabled = true)
+
+	protected static class GlobalSecurityConfiguration extends GlobalMethodSecurityConfiguration {
+
+	   @Override
+
+	   protected MethodSecurityExpressionHandler createExpressionHandler() {
+
+	       return new OAuth2MethodSecurityExpressionHandler();
+
+	   }
 	}
 }
--- src/main/java/com/tv/tutorials/coolestuffvoorcheops/controllers/MessageOfTheDayController.java
+++ src/main/java/com/tv/tutorials/coolestuffvoorcheops/controllers/MessageOfTheDayController.java
@@ -2,10 +2,13 @@ package com.tv.tutorials.coolestuffvoorcheops.controllers;
 
 import java.security.Principal;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class MessageOfTheDayController {
 	@GetMapping("/mod")
+	 @PreAuthorize("hasAuthority('Everyone') || #oauth2.hasScope('email')")
 	   public String getMessageOfTheDay(Principal principal) {
 	       return "The message of the day is boring for user: " + principal.getName();
 	   }
