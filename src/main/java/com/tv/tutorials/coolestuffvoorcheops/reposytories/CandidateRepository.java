0dda3a11c05e0b4528b394259f925099c991b007
yannick-at-778102206187 <qYVkNwQeXopkSpNlSTFnZ/SgDt087TBKOO4OW9lzgEw=>
2018-10-01 15:33:39 +0200

    Organized imports - YP

--- src/main/java/com/tv/tutorials/coolestuffvoorcheops/reposytories/CandidateRepository.java
+++ src/main/java/com/tv/tutorials/coolestuffvoorcheops/reposytories/CandidateRepository.java
@@ -1,24 +1,25 @@
 package com.tv.tutorials.coolestuffvoorcheops.reposytories;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.tv.tutorials.coolestuffvoorcheops.model.ApplicationProcess;
 import com.tv.tutorials.coolestuffvoorcheops.model.Candidate;
 
-public interface CandidateRepository  extends CrudRepository<Candidate, Integer>  {
-	//List<Address> findByStreetNameAndPostalCode(String streetName, String postalCode);
-	//List<Candidate> findAllByFirstNameLikeOrLastNameLike(String name, String sirName);
-	//findAllByFirstNameLikeOrLastNameLike
+public interface CandidateRepository extends CrudRepository<Candidate, Integer> {
+	// List<Address> findByStreetNameAndPostalCode(String streetName, String
+	// postalCode);
+	// List<Candidate> findAllByFirstNameLikeOrLastNameLike(String name, String
+	// sirName);
+	// findAllByFirstNameLikeOrLastNameLike
 
 	List<Candidate> findAllByNameLikeOrSirNameLike(String name, String sirName);
-	//List<Candidate>  findAllByApplicationProcessIdGreaterThan(int i) ;//w    findallByapplicationProcessId(<0)
-	//List<Candidate> findByAgeIn(Collection<Age> ages)
-	//List<ApplicationProcess> finAlldByIsRecrutedGreaterThan(int i);
+	// List<Candidate> findAllByApplicationProcessIdGreaterThan(int i) ;//w
+	// findallByapplicationProcessId(<0)
+	// List<Candidate> findByAgeIn(Collection<Age> ages)
+	// List<ApplicationProcess> finAlldByIsRecrutedGreaterThan(int i);
 
-	//Iterable<Candidate> findAllByskillsIdIn(List<Integer> skillsId);
+	// Iterable<Candidate> findAllByskillsIdIn(List<Integer> skillsId);
 
 	Iterable<Candidate> findAllBySkillsIdIn(List<Integer> skillsIds);
 
@@ -26,11 +27,11 @@ public interface CandidateRepository  extends CrudRepository<Candidate, Integer>
 
 	Candidate findByapplicationProcessId(Integer id);
 
-	//@overload
-	//ArrayList<Candidate> findAllByApplicationProcessIdIn(List<ApplicationProcess> applicationProcessIdsRecruited);
+	// @overload
+	// ArrayList<Candidate> findAllByApplicationProcessIdIn(List<ApplicationProcess>
+	// applicationProcessIdsRecruited);
 
-	//Iterable<Candidate> findAllBySkillsIdIn(List<Integer> skillsIds);
-	//Page<Candidate> findAll(Pageable pageRequest);
-	  
-	
+	// Iterable<Candidate> findAllBySkillsIdIn(List<Integer> skillsIds);
+	// Page<Candidate> findAll(Pageable pageRequest);
+
 }
