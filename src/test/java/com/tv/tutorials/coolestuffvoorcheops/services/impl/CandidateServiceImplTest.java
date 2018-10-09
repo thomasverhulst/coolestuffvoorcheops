f0d2ef987a488f52fd5ae6bd9a16f37396d8745a
yannick-at-778102206187 <qYVkNwQeXopkSpNlSTFnZ/SgDt087TBKOO4OW9lzgEw=>
2018-10-09 10:45:00 +0200

    Added tests for CandidateService - YP

--- src/test/java/com/tv/tutorials/coolestuffvoorcheops/services/impl/CandidateServiceImplTest.java
+++ src/test/java/com/tv/tutorials/coolestuffvoorcheops/services/impl/CandidateServiceImplTest.java
@@ -10,19 +10,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.tv.tutorials.coolestuffvoorcheops.models.Candidate;
 import com.tv.tutorials.coolestuffvoorcheops.models.CandidateSearchResolver;
+import com.tv.tutorials.coolestuffvoorcheops.services.ICandidateService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class CandidateServiceImplTest {
 
+	private static final String NAME = "Thomas";
+	private static final String SURNAME = "Verhulst";
+	private static final Integer ID = 1;
+
 	@Autowired
-	private CandidateService candidateService;
+	private ICandidateService candidateService;
 
 	@Test
 	public void getAllCandidatesTest() {
 		List<CandidateSearchResolver> listCandidates = candidateService.getAllCandidates();
 		assertThat(!listCandidates.isEmpty());
+	}
+
+	@Test
+	public void findAllByNameLikeOrSirNameLikeTestFilledNameAndSurname() {
+		List<CandidateSearchResolver> listCandidates = candidateService.findAllByNameLikeOrSirNameLike(NAME, SURNAME);
+		assertThat(listCandidates.size() >= 1);
+	}
+
+	@Test
+	public void findAllByNameLikeOrSirNameLikeTestWithoutNameAndSurname() {
+		List<CandidateSearchResolver> listCandidates = candidateService.findAllByNameLikeOrSirNameLike("", "");
+		assertThat(listCandidates.size() >= 1);
+	}
+
+	@Test
+	public void getCandidateByIdTest() {
+		Candidate candidate = candidateService.getCandidateById(ID);
+		assertThat(candidate != null);
 	}
 
 }
