af647bdf32f582743ca30c035c08be670a4f0c65
yannick-at-778102206187 <qYVkNwQeXopkSpNlSTFnZ/SgDt087TBKOO4OW9lzgEw=>
2018-10-17 16:21:01 +0200

    Created Custom login screen, where only admin can log in - YP

--- src/main/java/com/tv/tutorials/coolestuffvoorcheops/configuration/SecurityConfiguration.java
+++ src/main/java/com/tv/tutorials/coolestuffvoorcheops/configuration/SecurityConfiguration.java
@@ -32,7 +32,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.authorizeRequests().antMatchers("/").authenticated().anyRequest().permitAll().and().formLogin()
-				.permitAll();
+				.loginPage("/login").permitAll();
 	}
 
 	private PasswordEncoder getPasswordEncoder() {
